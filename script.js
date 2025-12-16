6cc6d674857ad7222c98f83d86984c0a752ba31d
sushma-regidi <regidisushma@gmail.com>
2025-12-16 13:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,79 +1,33 @@
-class MinHeap {
-    constructor() {
-        this.heap = [];
-    }
+function minCost(arr) {
+    if (!arr || arr.length <= 1) return 0;
 
-    push(val) {
-        this.heap.push(val);
-        this._bubbleUp(this.heap.length - 1);
-    }
-
-    pop() {
-        if (this.heap.length === 1) return this.heap.pop();
-        const min = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this._bubbleDown(0);
-        return min;
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    _bubbleUp(index) {
-        while (index > 0) {
-            let parent = Math.floor((index - 1) / 2);
-            if (this.heap[parent] <= this.heap[index]) break;
-            [this.heap[parent], this.heap[index]] =
-                [this.heap[index], this.heap[parent]];
-            index = parent;
-        }
-    }
-
-    _bubbleDown(index) {
-        const length = this.heap.length;
-        while (true) {
-            let left = 2 * index + 1;
-            let right = 2 * index + 2;
-            let smallest = index;
-
-            if (left < length && this.heap[left] < this.heap[smallest]) {
-                smallest = left;
-            }
-            if (right < length && this.heap[right] < this.heap[smallest]) {
-                smallest = right;
-            }
-            if (smallest === index) break;
-
-            [this.heap[index], this.heap[smallest]] =
-                [this.heap[smallest], this.heap[index]];
-            index = smallest;
-        }
-    }
-}
-
-function mincost(arr) {
-    if (arr.length <= 1) return 0;
-
-    const heap = new MinHeap();
-
-    for (let rope of arr) {
-        heap.push(rope);
-    }
+    // Sort array
+    arr.sort((a, b) => a - b);
 
     let totalCost = 0;
 
-    while (heap.size() > 1) {
-        let first = heap.pop();
-        let second = heap.pop();
-        let cost = first + second;
+    while (arr.length > 1) {
+        // Take two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
 
-        totalCost += cost;
-        heap.push(cost);
+        let sum = first + second;
+        totalCost += sum;
+
+        // Insert back while keeping array sorted
+        let index = arr.findIndex(x => x > sum);
+        if (index === -1) {
+            arr.push(sum);
+        } else {
+            arr.splice(index, 0, sum);
+        }
     }
 
     return totalCost;
 }
+
+module.exports = minCost;
+
 
 
 
